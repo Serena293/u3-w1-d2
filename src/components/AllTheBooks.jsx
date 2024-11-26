2009f8806ac63c8e5d8266de6ddba3484cec23be
SERENA FERRARO <serenaferraro21@gmail.com>
2024-11-26 14:37:58 +0100

    display books

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,23 +1,26 @@
+import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import horror from "../books/history.json";
 
 const AllTheBooks = (props) => {
-  horror.map((book) => {
-    return (
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    );
-  });
+  return (
+    <div className="d-flex flex-wrap">
+      {horror.map((book) => (
+        <Card key={book.asin} style={{ width: "18rem", margin: "10px" }}>
+          <Card.Img variant="top" src={book.img} alt={book.title} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Add to Cart</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default AllTheBooks;
